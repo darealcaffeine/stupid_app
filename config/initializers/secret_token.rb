06c48649eefcd767c20b24c6e7c5126f90fb3479
Kevin Lau <darealcaffeine@gmail.com>
2011-07-07 20:39:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StupidApp::Application.config.secret_token = '28f43cb905c487ee45324ade3ccf455a5adb442d818020adf4bff9a66beb4ee15380e75fcb125725d4df6b7def1e3f2bc3ae6501da71a832a44172597756267e'
